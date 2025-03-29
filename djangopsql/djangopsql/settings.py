1549cd650e548b25f7bb0505c597e1ea0678dc21
ANAS MUFTI <muftianas97@gmail.com>
2025-03-29 15:38:52 +0500

    ...

--- djangopsql/djangopsql/settings.py
+++ djangopsql/djangopsql/settings.py
@@ -61,7 +61,6 @@ WSGI_APPLICATION = 'djangopsql.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-
 DATABASES = {
     'default': {
          "ENGINE": "django.db.backends.postgresql",
@@ -75,7 +74,6 @@ DATABASES = {
         }
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
