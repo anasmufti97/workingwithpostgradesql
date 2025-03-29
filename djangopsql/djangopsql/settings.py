cd34e8d865bc74aad0162d01c87639f73d98de13
ANAS MUFTI <muftianas97@gmail.com>
2025-03-29 15:37:44 +0500

    ...

--- djangopsql/djangopsql/settings.py
+++ djangopsql/djangopsql/settings.py
@@ -61,6 +61,7 @@ WSGI_APPLICATION = 'djangopsql.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
          "ENGINE": "django.db.backends.postgresql",
